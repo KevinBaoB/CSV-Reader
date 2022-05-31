370850a45a165dd87133d0562c0c5e24b9b9c0fa
KevinBaoB <kevinbaobui@gmail.com>
2022-05-31 14:04:07 -0500

    Helped

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+animal = input('Enter an animal: ')
 
+try:
+    with open(f"data/{animal}.csv") as animals:
+        for line in animals.readlines()[1:]:
+            arr = line.strip().split(', ')
+            print(f"{arr[0]} is {arr[1]} year old {arr[2]}")
+except Exception:
+    print(f'Sorry we dont have {animal}')
